ecd4cc607197b47c85a88428b5648b1d331ce1de
bk2002-afk <71709655+bk2002-afk@users.noreply.github.com>
2020-11-10 14:18:13 +0400

    added some variables

--- Flappy Bird but Multiplayer/public/backup/birdbackup.js
+++ Flappy Bird but Multiplayer/public/backup/birdbackup.js
@@ -6,3 +6,13 @@ var pipeSouth = new Image();
 
 bird.src = "images/flappybird.png";
 bg.src = "images/flappybirdbackground.png";
+
+var gap = 85;
+var constant;
+
+var bX = 10;
+var bY = 150;
+
+var gravity = 1.5;
+
+var score = 0;
