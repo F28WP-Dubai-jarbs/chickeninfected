a9a9f112b9d5cccd2c3da08277639c05860a471f
bk2002-afk <71709655+bk2002-afk@users.noreply.github.com>
2020-11-10 14:10:04 +0400

    added backup

---
+++ Flappy Bird but Multiplayer/public/backup/birdbackup.js
@@ -0,0 +1,11 @@
+var bird = new Image();
+var bg = new Image();
+var fg = new Image();
+var pipeNorth = new Image();
+var pipeSouth = new Image();
+
+bird.src = "images/flappybird.png";
+bg.src = "images/bg.png";
+fg.src = "images/fg.png";
+pipeNorth.src = "images/pipeNorth.png";
+pipeSouth.src = "images/pipeSouth.png";
