02bc779cd51669f1478eaee5050b9316806ed907
Jishnu <71718745+jishnu3000@users.noreply.github.com>
2020-11-11 20:18:01 +0400

    edited sendPositionUpdates()

--- Flappy Bird but Multiplayer/public/app.js
+++ Flappy Bird but Multiplayer/public/app.js
@@ -269,6 +269,22 @@ var text = "The temperature is " + x + " Celsius";
   }
 
   function sendPositionUpdates() {
+    function jump(){
+    jumping = 1;
+    let jumpCount = 0;
+    var jumpInterval = setInterval(function(){
+        var characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+        if((characterTop>6)&&(jumpCount<15)){
+            character.style.top = (characterTop-5)+"px";
+        }
+        if(jumpCount>20){
+            clearInterval(jumpInterval);
+            jumping=0;
+            jumpCount=0;
+        }
+        jumpCount++;
+    },10);
+}
     
   }
 
